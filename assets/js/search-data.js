134b80d554792e24655d77645d31338356a95f94
VikramMahtoDesign <mahtovikram99@gmail.com>
2025-12-19 04:43:51 +0000

    Deploying to gh-pages from @ VikramMahtoDesign/vikrammahtodesign.github.io@96e735d701113ba3694e073e029b9c5442c9bcad 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,19 +9,19 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "Blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-experience",
+  },{id: "nav-experience",
           title: "Experience",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/experience/";
+          },
+        },{id: "nav-blog",
+          title: "Blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
           },
         },{id: "nav-projects",
           title: "Projects",
